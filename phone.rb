c0bcd8a09cc8a89346cc7a6934d49ae6ee22fd30
dmorris4 <dmorris4@binghamton.edu>
2015-02-10 23:54:21 -0500

    Added Questions 2 and 5

---
+++ phone.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+#phone.rb
+
+class Phone
+	def initialize(pattern, input, output1, output2)
+		@pattern = pattern
+		@input = input
+		@output1 = output1
+		@output2 = output2
+	end
+	
+	
+end
